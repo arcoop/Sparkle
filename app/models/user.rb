0ae3a13452d20bb144edf48fa961fde680b1ad23
acoop68@gmail.com <acoop68@gmail.com>
2022-10-07 11:58:57 -0400

    Write user auth and seed data

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_validation :ensure_session_token
+
+  validates :username, length: {in: 3..30}, format: {without: URI::MailTo::EMAIL_REGEXP, message: "username can't be email" }
+  validates :email, length: {in: 3..255}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: {in: 6..255}, allow_nil: true
+
+  def self.find_by_credentials(credential, pwd)
+    user = User.find_by(credential.match(URI::MailTo::EMAIL_REGEXP) ? {email: credential} : {username: credential})
+    user&.authenticate(pwd) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def generate_unique_session_token
+    loop do
+      token = SecureRandom::urlsafe_base64
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+end
