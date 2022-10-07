c53e1d65274f5cc615449512bf8c0693007d0c45
acoop68@gmail.com <acoop68@gmail.com>
2022-10-07 12:15:52 -0400

    Test login and logout methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,42 @@
 class ApplicationController < ActionController::API
     before_action :snake_case_params
 
-    private
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
 
+    def login!(user)
+        @current_user = user
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!(user)
+        user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: {message: 'Unauthorized'}, status: :unauthorized
+        end
+    end
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!(User.first)
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'username', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+      end
+
+    private
     def snake_case_params
         params.deep_transform_keys!(&:underscore)
     end
