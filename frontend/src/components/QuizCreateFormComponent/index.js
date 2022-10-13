49c3510825a9c0e1629d7d294d8a7e017b5997fc
acoop68@gmail.com <acoop68@gmail.com>
2022-10-13 12:09:33 -0400

    Reinstall node modules

--- frontend/src/components/QuizCreateFormComponent/index.js
+++ frontend/src/components/QuizCreateFormComponent/index.js
@@ -1,43 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
-// import { Redirect, useHistory } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { createQuiz } from '../../store/quizzes';
 import './QuizCreateForm.css'
-// import { useHistory } from "react-router-dom/cjs/react-router-dom"
-
 
 const QuizForm = ({props}) => {
     const [quizName, setQuizName] = useState("")
     const [quizType, setQuizType] = useState("Classic")
     const quizTypes = ["Classic", "Clickable", "Grid", "Map", "Picture Box", "Picture Click", "Slideshow"]
     const dispatch = useDispatch();
-    // const [redirect, setRedirect] = useState(false)
-    // const navigate = useNavigate()
-    // const history = useHistory();
-    // redirect state defaults to false
-
-    // useEffect(() => {
-    //     <Redirect to={"/"}/>
-    // }, [redirect])
-    console.log(props)
-
+    const [redirect, setRedirect] = useState(false)
+    const [id, setId] = useState(null)
+    
     const handleSubmit = (e) => {
         e.preventDefault()
         const quiz = {title: quizName, quizType: quizType}
         return dispatch(createQuiz(quiz))
             .then(async data => {
                 console.log(data)
-                const id = Object.values(data)[0].id
-                console.log(id)
+                // console.log(typeof parseInt(Object.keys(data)[0]))
+                setId(parseInt(Object.keys(data)[0]))
+                // console.log(id)
                 // history.push(`/create/edit/${id}`)
-                // setRedirect(true)
+                setRedirect(true)
             })
-        }
-        // return <Redirect to={`/create/edit/${id}`} />
-        
+    }
 
-        //redirect if redirect is true
-
+        if (redirect) return <Redirect to={`/create/edit/${id}`} />
+    
     return (
         <div id='quiz-form-container'>
 
--- frontend/src/components/QuizEditFormComponent/index.js
+++ frontend/src/components/QuizEditFormComponent/index.js
@@ -1,6 +1,13 @@
 import './QuizEditForm.css'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { getQuiz } from '../../store/quizzes'
 
 const QuizEditForm = () => {
+    const {quizId} = useParams()
+    const quiz = useSelector(getQuiz(quizId))
+
+
     
     return (
         <p>hi from quiz edit form</p>
