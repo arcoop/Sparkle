80ae3f9b3a49b62c1ed933fe1221fa153a282c2f
acoop68@gmail.com <acoop68@gmail.com>
2022-10-31 07:51:34 -0400

    Update search results returns

--- frontend/src/components/SearchResultsComponent/SearchResults.js
+++ frontend/src/components/SearchResultsComponent/SearchResults.js
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom"
 import { searchQuizzes } from "../../store/quizzes";
 import Navigation from "../Navigation";
 import Footer from "../Navigation/Footer";
+import { Link } from "react-router-dom";
 
 const SearchResults = () => {
     const dispatch = useDispatch();
@@ -18,13 +19,22 @@ const SearchResults = () => {
 
     const quizzes = useSelector(state => Object.values(state.quizzes))
 
+    let author;
+    let category;
+
     return (
    
             <div>
                 <ul>
                     {quizzes.map(quiz => {
+                        author = quiz.author;
+                        category = quiz.category
                         return (
-                            <li key={quiz.id}>{quiz.title} by {quiz.author.username}</li>
+                            <li key={quiz.id}>
+                                <Link to={`/quizzes/${quiz.id}`}>{quiz.title}</Link>
+                                <p>{quiz.description}</p>
+                                <p>by <Link to={`/users/${author.id}`}>{author.username}</Link> in <Link to={`/categories/${category.id}`}>{category.name}</Link></p>
+                            </li>
                         )
                     })}
                 </ul>
