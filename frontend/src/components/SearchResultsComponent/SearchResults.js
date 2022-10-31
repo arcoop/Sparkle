1689d759e9a7c6dd22f2eb53ea006ab24623a8ba
acoop68@gmail.com <acoop68@gmail.com>
2022-10-31 08:09:29 -0400

    Start formatting search results page

--- frontend/src/components/SearchResultsComponent/SearchResults.js
+++ frontend/src/components/SearchResultsComponent/SearchResults.js
@@ -5,6 +5,7 @@ import { searchQuizzes } from "../../store/quizzes";
 import Navigation from "../Navigation";
 import Footer from "../Navigation/Footer";
 import { Link } from "react-router-dom";
+import './SearchResults.css'
 
 const SearchResults = () => {
     const dispatch = useDispatch();
@@ -18,26 +19,36 @@ const SearchResults = () => {
     }, [query])
 
     const quizzes = useSelector(state => Object.values(state.quizzes))
-
+    const numQuizzes = quizzes.length
+    let resultsText = numQuizzes === 1 ? "result" : "results"
     let author;
     let category;
 
     return (
-   
-            <div>
-                <ul>
-                    {quizzes.map(quiz => {
-                        author = quiz.author;
-                        category = quiz.category
-                        return (
-                            <li key={quiz.id}>
-                                <Link to={`/quizzes/${quiz.id}`}>{quiz.title}</Link>
-                                <p>{quiz.description}</p>
-                                <p>by <Link to={`/users/${author.id}`}>{author.username}</Link> in <Link to={`/categories/${category.id}`}>{category.name}</Link></p>
-                            </li>
-                        )
-                    })}
-                </ul>
+            <div className="page-wrapper">
+                <Navigation />
+                <div className="search-results-container">
+                    <div className="search-results-heading">
+                        <h4>Quizzes</h4>
+                        <p>{numQuizzes} {resultsText} for "{query}"</p>
+                    </div>
+                    <div className="search-results">
+                        <ul className="results-list">
+                            {quizzes.map(quiz => {
+                                author = quiz.author;
+                                category = quiz.category
+                                return (
+                                    <li className="results-list-item" key={quiz.id}>
+                                        <Link className="result-title" to={`/quizzes/${quiz.id}`}>{quiz.title}</Link>
+                                        <p className="result-description">{quiz.description}</p>
+                                        <p className="result-author-category">by <Link className="result-info" to={`/users/${author.id}`}>{author.username}</Link> in <Link className="result-info" to={`/categories/${category.id}`}>{category.name}</Link></p>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+                <Footer />
             </div>
    
     )
