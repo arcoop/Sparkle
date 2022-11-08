77fe12a69c4fa2bd68d9efb06b3426434fd7054e
acoop68@gmail.com <acoop68@gmail.com>
2022-11-08 09:28:03 -0500

    add fetch random quiz id to quizzes store

--- frontend/src/store/quizzes.js
+++ frontend/src/store/quizzes.js
@@ -82,6 +82,12 @@ export const searchQuizzes = query => async dispatch => {
     dispatch(receiveQuizzes(data))
 }
 
+export const fetchRandomQuizID = () => async dispatch => {
+    const res = await csrfFetch(`/api/random/quizzes`)
+    const data = await res.json()
+    
+}
+
 
 export const fetchQuiz = quizId => async dispatch => {
     const res = await csrfFetch(`/api/quizzes/${quizId}`)
