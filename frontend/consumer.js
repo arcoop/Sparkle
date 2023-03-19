dc21601d1075770a4cbe6682ebe4044bd22048da
acoop68@gmail.com <acoop68@gmail.com>
2023-03-19 13:26:06 -0400

    add chat room frontend component

--- frontend/consumer.js
+++ frontend/consumer.js
@@ -1,2 +1,8 @@
 import { createConsumer } from "@rails/actioncable"
 export default createConsumer()
+
+let wsUrl;
+if (process.env.NODE_ENV !== "production") 
+  wsUrl = "ws://localhost:5000/cable";
+else
+  wsUrl = "/cable";
--- frontend/src/components/ChatComponents/Room.js
+++ frontend/src/components/ChatComponents/Room.js
@@ -1,32 +1,98 @@
-import consumer from '../consumer';
+import { useRef, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-class Room extends React.Component {
-  // ...
-  enterRoom() {
-    // ...
-    // Add the following lines to create a subscription:
-    this.subscription = consumer.subscriptions.create(
-      { channel: 'RoomsChannel', id: roomId },
+const Room = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [body, setBody] = useState('');
+  const [usersInRoom, setUsersInRoom] = useState({});
+
+  const {id: roomId} = useParams();
+  const messages = useSelector(getMessages(roomId));
+  const currentUserId = useSelector(state => state.currentUserId)
+  const room = useSelector(state => state.rooms[roomId]);
+
+  const activeMessagesRef = useRef(null);
+  const messageUIRef = useRef(null);
+  const prevRoom = useRef(null);
+  const numMessages = useRef(0);
+
+  const activeMessageId = parseInt(history.location.hash.slice(1));
+  const usersInRoomArray = Object.values(usersInRoom);
+
+  useEffect (() => {
+    if (activeMessagesRef.current) scrollToMessage();
+  }, [activeMessageId])
+
+  useEffect(() => {
+    if (roomId === prevRoom.current && numMessages.current < messages.length) {
+      if (history.location.hash) history.push(history.location.pathname);
+      scrollToBottom();
+    }
+    numMessages.current = messages.length;
+  }, [messages, RoomId, history])
+
+  useEffect(() => {
+    dispatch(fetchRoom(roomId)).then(() => {
+      if (activeMessageRef.current) {
+        scrollToMessage();
+      } else {
+        scrollToBottom();
+      }
+      prevRoom.current = roomId;
+    });
+    const subscription = consumer.subscriptions.create(
+      {channel: 'RoomsChannel', id: roomId},
       {
-        received: message => {
-          console.log('Received message: ', message)
+        received: ({message, user}) => {
+          dispatch(receiveMessage(message));
+          dispatch(receiveUser(user));
         }
       }
     );
+    return () => subscription?.unsubscribe();
+  }, [roomId, dispatch])
+
+  const scrollToMessage = () => {
+    activeMessagesRef.current.focus();
+    activeMessagesRef.current.scrollIntoView();
   }
 
-  componentDidUpdate(/* ...destructured prevProps... */) {
-    // ...
-    if (prevRoomId !== curRoomId) {
-      // Add this line to unsubscribe from previous room upon switching rooms:
-      this.subscription?.unsubscribe();
-      this.enterRoom();
-    } // ...
+  const scrollToBottom = () => {
+    messageUIRef.current.scrollTop = messageUIRef.current.scrollHeight;
   }
 
-  // Add a componentWillUnmount to unsubscribe on leaving the room show page:
-  componentWillUnmount() {
-    this.subscription?.unsubscribe();
+  // const setReaction = (id, reaction) => {
+  //   setUsersInRoom(prevUsersInRoom => ({...prevUsersInRoom, [id]: { ...prevUsersInRoom[id], reaction } }))
+  // }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    createMessage({ body, roomId, authorId: currentUserId}).then(() => {
+      setBody(' ')
+    })
   }
-  // ...
+
+  
+  return (
+    <div>
+      Room!
+      <form onSubmit={handleSubmit}>
+        <textarea 
+          rows={body.split('\n').length}
+          onChange={e => setBody(e.target.value)}
+          onKeyDown={e => {
+            if (e.code === 'Enter' && !e.shiftKey) {
+              handleSubmit(e);
+            }
+          }}
+          value={body}
+        />
+
+      </form>
+    </div>
+  )
 }
+
+export default Room;
